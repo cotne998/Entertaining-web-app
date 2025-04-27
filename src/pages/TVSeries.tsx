1227e999d060e27e2942bd23e24adafedf6987f5
cotne998 <cotnecincadze998@gmail.com>
2025-04-28 00:48:11 +0400

    TV Series page

--- src/pages/TVSeries.tsx
+++ src/pages/TVSeries.tsx
@@ -1,3 +1,62 @@
+import {
+  RecommendedContainer,
+  Title,
+  RecommendedInfo,
+  RecommendedContent,
+  RecommendedItem,
+  Thumbnail,
+  ThumbnailDesktop,
+  MovieInfo,
+  CategoryIcon,
+  MovieTitle,
+} from "../components/Recommended";
+import { Bookmark } from "../components/TrendingComponent";
+import { BookmarkIcon } from "../components/TrendingComponent";
+import { useContext } from "react";
+import { dataContext } from "./MainLayout";
+
 export default function TVSeries() {
-  return <div></div>;
+  const { dataState, handleBookmark } = useContext(dataContext);
+
+  return (
+    <>
+      <RecommendedContainer>
+        <Title>Movies</Title>
+        <RecommendedContent>
+          {dataState.map((item, index) => {
+            return (
+              item.category === "TV Series" && (
+                <>
+                  <RecommendedItem key={index}>
+                    <Bookmark onClick={() => handleBookmark(index)}>
+                      <BookmarkIcon
+                        src={
+                          item.isBookmarked
+                            ? "/assets/icon-bookmark-full.svg"
+                            : "/assets/Bookmark.svg"
+                        }
+                      />
+                    </Bookmark>
+                    <Thumbnail src={item.thumbnail.regular.small} />
+                    <ThumbnailDesktop src={item.thumbnail.regular.large} />
+                    <RecommendedInfo>
+                      <MovieInfo>
+                        {item.year} •{" "}
+                        <CategoryIcon
+                          src="/assets/icon-nav-tv-series.svg"
+                          alt=""
+                        />{" "}
+                        {item.category} • {item.rating}
+                      </MovieInfo>
+                      <MovieTitle>{item.title}</MovieTitle>
+                    </RecommendedInfo>
+                  </RecommendedItem>
+                </>
+              )
+            );
+          })}
+        </RecommendedContent>
+      </RecommendedContainer>
+    </>
+  );
 }
